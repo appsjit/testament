fcd9f25dd303f9c673f280dc8d7b488c3d9636a6
Jeet <appsjit@gmail.com>
2019-01-11 01:32:34 -0800

    Correct Answer

--- LeetCode/soljit/002_AddingNumbers.py
+++ LeetCode/soljit/002_AddingNumbers.py
@@ -5,6 +5,8 @@
 #         self.val = x
 #         self.next = None
 
+
+
 class Solution:
     def getNm(l): x=""
         childNode = l
@@ -19,8 +21,8 @@ class Solution:
         return int(x)
 
     def addTwoNumbers(self, l1, l2):
-        resNum = Solution.getNm(l1) + Solution.getNm(l2)
-        return list(map(int, str(resNum)[::-1]))
+        # resNum = Solution.getNm(l1) + Solution.getNm(l2)
+        # return list(map(int, str(resNum)[::-1]))
 
         """
 
@@ -31,4 +33,24 @@ class Solution:
         :rtype: ListNode
 
         """
+        hatcha = 0
+        result = list()
+        while l1 or l2 or hatcha:
+            val1 = l1.val if l1 else 0
+            l1 = l1.next if l1 else 0
+
+            l2,val2=[l2. next , l2.val] i f l2 else [0,0]
+            hatcha , baki =divmod(val1 + val2 + hatcha , 10)
+            result.append(baki)
+
+        return result
+
+
+
+
+
+
+
+
+
 
